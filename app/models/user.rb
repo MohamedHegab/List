c6c0aceeb01cc491482f3b5322cf3f6ef88f664d
Mohamed Hegab <mohamedhegab92@gmail.com>
2018-09-23 14:54:35 +0200

    assign user role

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  ############## Callbacks #################
+  before_create :assign_role
 
   ############## Validations #################
   validates_presence_of :username
@@ -11,4 +13,10 @@ class User < ApplicationRecord
   validates_uniqueness_of :username, :email
 
   enum role_input: [:member, :admin]
+
+  def assign_role
+    if self.role_input && !self.has_role?(self.role_input)
+      self.add_role role_input
+    end
+  end
 end
